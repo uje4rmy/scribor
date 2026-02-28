b13a55cd8aca3d1f5a25fe74826aa8064bbc2db1
ian-ngu <155700654+ian-ngu@users.noreply.github.com>
2026-02-28 16:10:46 +1100

    Fixed tailwind errors

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,5 +1,4 @@
 import path from "path";
-import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import { fileURLToPath } from "url";
@@ -9,7 +8,7 @@ const __dirname = path.dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
