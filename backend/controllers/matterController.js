8a26e48258f96daafdce10398f65805d117c0014
ian-ngu <155700654+ian-ngu@users.noreply.github.com>
2026-03-08 23:53:00 +1100

    Added backend GET requests for client profiles Installed ulid to generate ids

--- backend/controllers/matterController.js
+++ backend/controllers/matterController.js
@@ -1,12 +1,32 @@
 const pool = require("../db");
+const { ulid } = require("ulid");
+
+function generateId(prefix) {
+  return `${prefix}_${ulid()}`;
+}
 
 module.exports = {
   getMatters: async (req, res) => {
     try {
       const id = req.params.userId;
       const query =
-        "SELECT * FROM CLIENT c JOIN MATTER m ON c.client_id = m.client_id WHERE user_id = (?)";
+        "SELECT c.client_id, c.user_id, c.client_fullname, m.matter_type, c.client_type, c.client_level, c.client_flagged, c.client_status FROM CLIENT c JOIN MATTER m ON c.client_id = m.client_id WHERE user_id = (?)";
       const [results] = await pool.query(query, [id]);
+
+      res.json(results);
+    } catch (error) {
+      console.error("GET Query Error: ", error);
+    }
+  },
+  getClientProfile: async (req, res) => {
+    try {
+      const id = req.params.userId;
+      const query =
+        "SELECT * FROM CLIENT c JOIN MATTER m ON c.client_id = m.client_id JOIN ENTITY e ON c.client_id = e.client_id WHERE user_id = (?)";
+      const [results] = await pool.query(query, [id]);
+
+      const formattedDob = results[0].client_dob.toISOString().slice(0, 10);
+      results[0].client_dob = formattedDob;
 
       res.json(results);
     } catch (error) {
--- backend/routes/matterRoutes.js
+++ backend/routes/matterRoutes.js
@@ -3,6 +3,11 @@ const router = express.Router();
 const matterController = require("../controllers/matterController");
 
 router.get("/matters/:userId", matterController.getMatters);
+router.get(
+  "/matters/client-profile/:userId",
+  matterController.getClientProfile,
+);
+
 router.post("/matters/update-status", matterController.postUpdateStatus);
 
 module.exports = router;
